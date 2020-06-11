26087c71a25d2583a5ecc3f01e5477cf8d576e6a
Yejin <s2018s05@e-mirim.hs.kr>
2020-06-11 14:46:48 +0900

    20200611 상속 예제

--- InheritancePrac/Program.cs
+++ InheritancePrac/Program.cs
@@ -2,14 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace InheritancePrac
+namespace inheritance
 {
     class Program
     {
         static void Main(string[] args)
         {
+            List<Dog> dog = new List<Dog>() { new Dog(), new Dog(), new Dog() };
+            List<Cat> cat = new List<Cat>() { new Cat(), new Cat(), new Cat() };
+
+            cat.Add(new Cat());
+            cat.Add(new Cat());
+            cat.Add(new Cat());
+
+            foreach (var item in dog)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Bark();
+            }
+            foreach (var item in cat)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Moew();
+            }
         }
     }
 }
